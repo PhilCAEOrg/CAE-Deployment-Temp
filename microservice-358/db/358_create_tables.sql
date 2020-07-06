7663924b656d4e6aba677321d3975f5c876c599b
PhilCAEUser <mail@rwth>
2020-07-06 13:41:11 +0000

    Added microservice 358

---
+++ microservice-358/db/358_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 358, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table todo_items.
+--
+CREATE TABLE caeschema.todo_items (
+  id INTEGER ,
+  item_name VARCHAR(255) ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
