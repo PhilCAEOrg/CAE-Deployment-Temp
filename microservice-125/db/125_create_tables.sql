ce4bf264f1eaf7eb3eb90414f405a26063c5255a
PhilCAEUser <mail@rwth>
2020-07-19 08:18:06 +0000

    Added microservice 125

---
+++ microservice-125/db/125_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 125, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab2.
+--
+CREATE TABLE caeschema.tab2 (
+  col INTEGER ,
+CONSTRAINT col_PK PRIMARY KEY (col)
+ 
+);
+
+
+
