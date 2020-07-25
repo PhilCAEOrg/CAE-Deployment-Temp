d20517b65514eee2db40dfe7d245bf1571b5dcce
PhilCAEUser <62888099+PhilCAEUser@users.noreply.github.com>
2020-07-25 16:34:30 +0200

    Update get_ext_dependencies.sh

--- get_ext_dependencies.sh
+++ get_ext_dependencies.sh
@@ -1,6 +1,6 @@
 mkdir dependencies
 cd dependencies
 
-mkdir dependency-0 && cd dependency-0
+mkdir "dependency-0" && cd "dependency-0"
 git clone https://github.com/PhilCAEOrg/microservice-131
 cd ..
