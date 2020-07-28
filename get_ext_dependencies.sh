03719621affdcd14134a2d22016d313feea5659b
PhilCAEUser <mail@rwth>
2020-07-28 11:14:03 +0000

    Initialized repository for deployment

---
+++ get_ext_dependencies.sh
@@ -0,0 +1,8 @@
+mkdir dependencies
+cd dependencies
+mkdir frontend && cd frontend
+git clone https://github.com/PhilCAEOrg/WidgetTest
+cd ..
+mkdir microservices && cd microservices
+git clone https://github.com/PhilCAEOrg/microservice-165
+cd ..
