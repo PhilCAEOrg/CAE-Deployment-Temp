43f70b46ca4efd55df83740eceb4ae25bdc1967f
las2peer <las2peer@e5d70b455018>
2020-08-03 08:47:22 +0000

    Clear

--- get_ext_dependencies.sh
+++
@@ -1,8 +0,0 @@
-mkdir dependencies
-cd dependencies
-mkdir frontend && cd frontend
-git clone https://github.com/PhilCAEOrg/WidgetTest
-cd ..
-mkdir microservices && cd microservices
-git clone https://github.com/PhilCAEOrg/microservice-165
-cd ..
